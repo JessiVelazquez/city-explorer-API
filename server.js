cc991575342f942b413408abb88f33fc6cad693f
Jessi <jessivelazq1@gmail.com>
2021-03-24 18:15:43 -0700

    got weather data from API to pass to site onSUbmit

--- server.js
+++ server.js
@@ -17,13 +17,13 @@ const cors = require('cors');
 // initalizing the express library so I can use it
 const app = express();
 
-const weather = require('./weather.json');
+const superagent = require('superagent');
 
 //this allows anyone to access our server - aka - the worlds worst body guard
 app.use(cors());
 
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 app.get('/', function (request, response) {
   response.send('Hello World');
 });
@@ -31,25 +31,46 @@ app.get('/', function (request, response) {
 
 app.get('/weather', forecastData);
 
-
 function forecastData(request, response) {
-  const city = weather.city_name;
-  const lat = weather.lat;
-  const lon = weather.lon;
-  try {
-    let array = weather.data.map(day => {
-      return new Forecast(day);
+  console.log(request.query);
+  const city = request.query.city_name;
+  // const lat = request.query.lat;
+  // const lon = request.query.lon;
+  const url = 'http://api.weatherbit.io/v2.0/forecast/daily';
+  const query = {
+    city: city,
+    key: process.env.WEATHER_API_KEY,
+  };
+
+  superagent
+    .get(url)
+    .query(query)
+    .then(superagentResults => {
+      const weatherArray = superagentResults.body.data.map(agent => {
+        console.log(agent);
+        return new Forecast(agent);
+      });
+      console.log(weatherArray);
+      response.status(200).send(weatherArray);
     });
-    const results = {
-      city: city,
-      lat: lat,
-      lon: lon,
-      forecast: array
-    };
-    response.status(200).json(results);
-  } catch (error) {
-    response.status(500).send('server error');
-  }
+
+
+
+  // ----------------------------
+  // try {
+  //   let array = weather.data.map(day => {
+  //     return new Forecast(day);
+  //   });
+  //   const results = {
+  //     city: city,
+  //     lat: lat,
+  //     lon: lon,
+  //     forecast: array
+  //   };
+  //   response.status(200).json(results);
+  // } catch (error) {
+  //   response.status(500).send('server error');
+  // }
 }
 
 
